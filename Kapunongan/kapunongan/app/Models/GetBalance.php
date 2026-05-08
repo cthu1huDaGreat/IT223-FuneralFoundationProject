26779c4bcdcfec340fc5c69429fe902cc7dbbd4d
Embodo <Erickgwapogs@gmail.com>
2026-05-08 08:33:39 +0800

    Add files via upload

---
+++ Kapunongan/kapunongan/app/Models/GetBalance.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GetBalance extends Model
+{
+    protected $table = 'getbalance';
+    public $timestamps = false;
+    protected $primaryKey = null;
+    public $incrementing = false;
+}
